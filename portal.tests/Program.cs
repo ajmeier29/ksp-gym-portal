3ab11de935114e9fd1483e912421a599da66de3b
Andrew Meier <ajmeier29@gmail.com>
2019-08-22 22:43:30 -0400

    Normalized yaml file and env var names

--- portal.tests/Program.cs
+++ portal.tests/Program.cs
@@ -11,17 +11,14 @@ namespace portal.tests
     {
         public static int Main(string[] args)
         {
-            var dbUser = Environment.GetEnvironmentVariable("DBUSER"); 
-            var systemdef = Environment.GetEnvironmentVariable("System.DefinitionId");
-            // IConfiguration _configuration = new ConfigurationBuilder()
-            //     // .AddCommandLine(_args)
-            //     .AddEnvironmentVariables("UNITTEST_CUSTOM_")
-            //     .Build();
-            // var test = _configuration["DBUSER"];
-            // var hostname = _configuration["HOSTNAME"];
-            // var password = _configuration["PASSWORD"];
-            Console.WriteLine($"Vars: {dbUser}");
-            Console.WriteLine($"System.DefinitionId: {systemdef}");
+            var mongoDbUser = Environment.GetEnvironmentVariable("MongoDbUser"); 
+            var mongoDbPassword = Environment.GetEnvironmentVariable("MongoDbPassword"); 
+            var mongoDbHostname = Environment.GetEnvironmentVariable("MongoDbHostname"); 
+            var mongoDbConnectionString = Environment.GetEnvironmentVariable("MongoDbConnectionString"); 
+            Console.WriteLine($"mongoDbUser: {mongoDbUser}");
+            Console.WriteLine($"mongoDbPassword: {mongoDbPassword}");
+            Console.WriteLine($"mongoDbHostname: {mongoDbHostname}");
+            Console.WriteLine($"mongoDbConnectionString: {mongoDbConnectionString}");
             // var test = new AutoRun(typeof(Program).GetTypeInfo().Assembly)
             //     .Execute(args, new ExtendedTextWrapper(Console.Out), Console.In);
             // MongoDb_IntegrationTests tests = new MongoDb_IntegrationTests(args);
